dcec07e5086ad4ccf0d49bebfcb382071c1ac05b
Ali <alialashwall14@gmail.com>
2023-10-22 18:48:00 +0300

    network api

---
+++ app/src/main/java/com/example/quizzard/network/QuizApiService.kt
@@ -0,0 +1,36 @@
+package com.example.quizzard.network
+
+import com.example.quizzard.data.QuizData
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://opentdb.com/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface QuizApiService {
+    @GET("api.php?amount=10&category=18")
+    suspend fun getComputerQuestions() : QuizData
+
+    @GET("api.php?amount=10&category=21&difficulty=easy")
+    suspend fun getSportsQuestion() : QuizData
+
+    @GET("api.php?amount=10&category=19&difficulty=medium")
+    suspend fun getMathQuestions() : QuizData
+
+    @GET("api.php?amount=10")
+    suspend fun getDailyQuiz() : QuizData
+
+}
+
+object QuizApi{
+    val retrofitService : QuizApiService by lazy {
+        retrofit.create(QuizApiService::class.java)
+    }
+}
